7258e629195f74754dfbf9c4df6aae3e45c8ca63
Mkheir13 <kheireddine.mederreg@ynov.com>
2023-02-06 11:01:15 +0100

    refactoring some files

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="src/style/index.css">
+    <link rel="stylesheet" href="src/style/sidebar.css">
     <link rel="stylesheet" href="src/style/footer.css">
     <title>MEDUSA</title>
+    <link rel="icon" type="image/png" href="src/img/medusa.png" />
 </head>
 <body>
 
+<?php require_once 'src/components/sidebar.html'; ?>
 <?php require_once 'src/components/footer.html'; ?>
 
 </body>
