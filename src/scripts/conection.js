d870725f5632cadb8e5a43a3af3968405a157f50
Mkheir13 <kheireddine.mederreg@ynov.com>
2023-02-07 11:56:46 +0100

    grotte medusa

--- src/scripts/conection.js
+++ src/scripts/conection.js
@@ -2,11 +2,16 @@ function validateForm() {
     var username = document.forms["loginForm"]["username"].value;
     var password = document.forms["loginForm"]["password"].value;
 
-    var conssct = document.querySelector("BNT_cheh");
+    var conssct= document.querySelector("BNT_cheh");
 
-    if (username == "admin" && password == "admin") {
-        conssct.addEventListener('click', () => {
-            window.location.href = "http://medusa48h.alwaysdata.net/grotte.php";
-        });
-    }
-}
+
+    conssct.addEventListener('click', () => {
+        if (username == "admin" && password == "admin") {
+            alert("Bienvenue");
+        }
+        window.location.href = "http://medusa48h.alwaysdata.net/grotte.php";
+        else {
+           this.reload();
+        }
+    });
+  }
