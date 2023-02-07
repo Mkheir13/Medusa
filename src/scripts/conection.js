0852065d124a935e69bf263a65bfd294c9226515
Mkheir13 <kheireddine.mederreg@ynov.com>
2023-02-07 11:40:40 +0100

    grotte medusa

--- src/scripts/conection.js
+++ src/scripts/conection.js
@@ -2,7 +2,7 @@ function validateForm() {
     var username = document.forms["loginForm"]["username"].value;
     var password = document.forms["loginForm"]["password"].value;
     if (username === "hola" && password === "poto") {
-      window.location = "nextpage.html";
+      window.location = "http://medusa48h.alwaysdata.net/grotte.php";
       return false;
     } else {
       alert("Nom d'utilisateur ou mot de passe incorrect");
