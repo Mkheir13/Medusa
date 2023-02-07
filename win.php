d3fe5d192661143cac7bc2d2c3d01086ea318d29
Mkheir13 <kheireddine.mederreg@ynov.com>
2023-02-07 12:24:55 +0100

    readme

---
+++ win.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="src/style/index.css">
+    <link rel="stylesheet" href="src/style/sidebar.css">
+    <link rel="stylesheet" href="src/style/footer.css">
+    <link rel="stylesheet" href="src/style/conection.css">
+    <link rel="stylesheet" href="src/style/grotte.css">
+    <link rel="stylesheet" href="src/style/win.css">
+    <script defer src="src/scripts/grotte.js"></script>
+    <title>MEDUSA</title>
+    <link rel="icon" type="image/png" href="src/img/medusa.png" />
+</head>
+<body>
+
+<?php require_once 'src/components/sidebar.html'; ?>
+<?php require_once 'src/components/win.html'; ?>
+<?php require_once 'src/components/footer.html'; ?>
+
+</body>
+</html>
